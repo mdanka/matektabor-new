cd3b2d00635f0b23874a05e93f650adaa7d0ec6c
Miklos Danka <danka.miklos@gmail.com>
2019-04-10 13:56:07 +0200

    Removed unused props from drawer

--- src/components/barkochba/barkochbaDrawer.tsx
+++ src/components/barkochba/barkochbaDrawer.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { IAppState, selectBarkochbaDrawerIsOpen, SetBarkochbaDrawerIsOpen } from "../../store";
+import { IAppState } from "../../store";
 import { Dispatch } from "redux";
 import { List, ListItemText, ListItem, Divider, ListItemIcon, Link } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
@@ -17,13 +17,9 @@ const BarkochbaManageLink = (props: any) => <RouterLink to={NavUtils.getNavUrl[P
 
 export interface IBarkochbaDrawerOwnProps {}
 
-export interface IBarkochbaDrawerStateProps {
-    isOpen: boolean;
-}
+export interface IBarkochbaDrawerStateProps {}
 
-export interface IBarkochbaDrawerDispatchProps {
-    setIsOpen: (isOpen: boolean) => void;
-}
+export interface IBarkochbaDrawerDispatchProps {}
 
 export type IBarkochbaDrawerProps = IBarkochbaDrawerOwnProps &
     IBarkochbaDrawerStateProps &
@@ -62,17 +58,12 @@ export class UnconnectedBarkochbaDrawer extends React.Component<IBarkochbaDrawer
     }
 }
 
-function mapStateToProps(state: IAppState, _ownProps: IBarkochbaDrawerOwnProps): IBarkochbaDrawerStateProps {
-    return {
-        isOpen: selectBarkochbaDrawerIsOpen(state),
-    };
+function mapStateToProps(_state: IAppState, _ownProps: IBarkochbaDrawerOwnProps): IBarkochbaDrawerStateProps {
+    return {};
 }
 
-function mapDispatchToProps(dispatch: Dispatch, _ownProps: IBarkochbaDrawerOwnProps): IBarkochbaDrawerDispatchProps {
-    return {
-        setIsOpen: (barkochbaDrawerIsOpen: boolean) =>
-            dispatch(SetBarkochbaDrawerIsOpen.create({ barkochbaDrawerIsOpen })),
-    };
+function mapDispatchToProps(_dispatch: Dispatch, _ownProps: IBarkochbaDrawerOwnProps): IBarkochbaDrawerDispatchProps {
+    return {};
 }
 
 export const BarkochbaDrawer = connect(
