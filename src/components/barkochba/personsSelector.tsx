5863671c90f7a4982aebbeb94a162282966b1e38
Miklos Danka <danka.miklos@gmail.com>
2019-04-16 15:00:04 +0200

    Now cannot remove people who listened to a story. Fixes #24

--- src/components/barkochba/personsSelector.tsx
+++ src/components/barkochba/personsSelector.tsx
@@ -4,6 +4,7 @@ import { ActionMeta, ValueType } from "react-select/lib/types";
 import { AutoCompleteSelector } from "./autoCompleteSelector";
 
 export interface IPersonsSelectorProps {
+    className?: string;
     allPersons: ISelectOption[];
     selectedPersons: ISelectOption[];
     onChange: (values: ISelectOption[], action: ActionMeta) => void;
@@ -12,9 +13,10 @@ export interface IPersonsSelectorProps {
 
 export class PersonsSelector extends React.Component<IPersonsSelectorProps, {}> {
     public render() {
-        const { allPersons, selectedPersons, disabled } = this.props;
+        const { allPersons, selectedPersons, disabled, className } = this.props;
         return (
             <AutoCompleteSelector
+                className={className}
                 options={allPersons}
                 value={selectedPersons}
                 onChange={this.handlePersonsWhoKnowChange}
