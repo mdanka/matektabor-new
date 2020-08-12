140d54dcfc42606f987c2e4c8e1a320c5d57afc7
Miklos Danka <8069999+mdanka@users.noreply.github.com>
2020-08-12 20:44:13 +0200

    Style and theme fixes

--- src/utils/themeUtils.ts
+++ src/utils/themeUtils.ts
@@ -3,11 +3,23 @@ import { createMuiTheme } from "@material-ui/core";
 export const DARK_THEME = createMuiTheme({
     palette: {
         type: "dark",
+        primary: {
+            main: "#ffc40d",
+        },
+        secondary: {
+            main: "#1363A5",
+        },
     },
 });
 
 export const LIGHT_THEME = createMuiTheme({
     palette: {
         type: "light",
+        primary: {
+            main: "#ffc40d",
+        },
+        secondary: {
+            main: "#1363A5",
+        },
     },
 });
