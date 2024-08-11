5ae54792b677a6b9b2d641c8872b48be085709db
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-11 16:23:42 +0200

    Convert to functional components

--- src/components/barkochba/barkochbaDrawer.tsx
+++ src/components/barkochba/barkochbaDrawer.tsx
@@ -1,7 +1,3 @@
-import * as React from "react";
-import { connect } from "react-redux";
-import { IAppState } from "../../store";
-import { Dispatch } from "redux";
 import { List, ListItemText, ListItem, Divider, ListItemIcon, Link } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import TableChartIcon from "@mui/icons-material/TableChart";
@@ -14,68 +10,47 @@ const BarkochbaExportLink = (props: any) => (
     <RouterLink to={getNavUrl[Page.BarkochbaExport](undefined)} {...props} />
 );
 
-const BarkochbaManageLink = (props: any) => <RouterLink to={getNavUrl[Page.BarkochbaManage]()} {...props} />;
+const BarkochbaManageLink = (props: any) => (
+    <RouterLink to={getNavUrl[Page.BarkochbaManage]()} {...props} />
+);
 
-export interface IBarkochbaDrawerOwnProps {}
-
-export interface IBarkochbaDrawerStateProps {}
-
-export interface IBarkochbaDrawerDispatchProps {}
-
-export type IBarkochbaDrawerProps = IBarkochbaDrawerOwnProps &
-    IBarkochbaDrawerStateProps &
-    IBarkochbaDrawerDispatchProps;
-
-export class UnconnectedBarkochbaDrawer extends React.Component<IBarkochbaDrawerProps, {}> {
-    public render() {
-        return (
-            <div className={css.barkochbaDrawer}>
-                <div className={css.barkochbaDrawerNavigation}>
-                    <List>
-                        <Link
-                            variant="body1"
-                            color="inherit"
-                            component={BarkochbaExportLink}
-                            underline="hover">
-                            <ListItem button className={css.barkochbaDrawerListItem}>
-                                <ListItemIcon>
-                                    <TableChartIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Áttekintő táblázatok" />
-                            </ListItem>
-                        </Link>
-                        <Link
-                            variant="body1"
-                            color="inherit"
-                            component={BarkochbaManageLink}
-                            underline="hover">
-                            <ListItem button className={css.barkochbaDrawerListItem}>
-                                <ListItemIcon>
-                                    <EditIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Táborok szerkesztése" />
-                            </ListItem>
-                        </Link>
-                    </List>
-                    <Divider />
-                </div>
-                <div className={css.barkochbaDrawerStoryBrowser}>
-                    <StoryBrowser />
-                </div>
+export function BarkochbaDrawer() {
+    return (
+        <div className={css.barkochbaDrawer}>
+            <div className={css.barkochbaDrawerNavigation}>
+                <List>
+                    <Link
+                        variant="body1"
+                        color="inherit"
+                        component={BarkochbaExportLink}
+                        underline="hover"
+                    >
+                        <ListItem button className={css.barkochbaDrawerListItem}>
+                            <ListItemIcon>
+                                <TableChartIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Áttekintő táblázatok" />
+                        </ListItem>
+                    </Link>
+                    <Link
+                        variant="body1"
+                        color="inherit"
+                        component={BarkochbaManageLink}
+                        underline="hover"
+                    >
+                        <ListItem button className={css.barkochbaDrawerListItem}>
+                            <ListItemIcon>
+                                <EditIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Táborok szerkesztése" />
+                        </ListItem>
+                    </Link>
+                </List>
+                <Divider />
             </div>
-        );
-    }
+            <div className={css.barkochbaDrawerStoryBrowser}>
+                <StoryBrowser />
+            </div>
+        </div>
+    );
 }
-
-function mapStateToProps(_state: IAppState, _ownProps: IBarkochbaDrawerOwnProps): IBarkochbaDrawerStateProps {
-    return {};
-}
-
-function mapDispatchToProps(_dispatch: Dispatch, _ownProps: IBarkochbaDrawerOwnProps): IBarkochbaDrawerDispatchProps {
-    return {};
-}
-
-export const BarkochbaDrawer = connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(UnconnectedBarkochbaDrawer);
--- src/components/staticContent.tsx
+++ src/components/staticContent.tsx
@@ -1,51 +1,36 @@
-import * as React from "react";
 import css from "./staticContent.module.scss";
 
-export interface IStaticContentProps {
+interface IStaticContentProps {
     type: "terms of service" | "privacy policy";
 }
 
-export class StaticContent extends React.Component<IStaticContentProps, {}> {
-    public render() {
-        const { type } = this.props;
-        return (
-            <div className={css.staticContent}>
-                {type === "terms of service" ? this.renderTermsOfService() : null}
-                {type === "privacy policy" ? this.renderPrivacyPolicy() : null}
-            </div>
-        );
-    }
+export function StaticContent({ type }: IStaticContentProps) {
+    const renderTermsOfService = () => (
+        <div>
+            <h1>Terms of Service ("Terms")</h1>
+            <p>Last updated: April 03, 2019</p>
+            <p>Azzal, hogy ezt a weblapot használod, nekünk adod a lelkedet.</p>
+            <p>Örökre.</p>
+            <p>De amúgy nem vállalunk semmiért se felelősséget.</p>
+        </div>
+    );
 
-    private renderTermsOfService = () => {
-        return (
-            <div>
-                <h1>Terms of Service ("Terms")</h1>
+    const renderPrivacyPolicy = () => (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>Effective date: April 03, 2019</p>
+            <p>Itt semmi sem privát.</p>
+            <p>
+                Az adataidat közvetlenül Oroszországba küldjük, illetve automatikusan feltöltjük a WikiLeaksre.
+                Szegény Clinton is hogy pórul járt miután bejelentkezett ide!
+            </p>
+        </div>
+    );
 
-                <p>Last updated: April 03, 2019</p>
-
-                <p>Azzal, hogy ezt a weblapot használod, nekünk adod a lelkedet.</p>
-
-                <p>Örökre.</p>
-
-                <p>De amúgy nem vállalunk semmiért se felelősséget.</p>
-            </div>
-        );
-    };
-
-    private renderPrivacyPolicy = () => {
-        return (
-            <div>
-                <h1>Privacy Policy</h1>
-
-                <p>Effective date: April 03, 2019</p>
-
-                <p>Itt semmi sem privát.</p>
-
-                <p>
-                    Az adataidat közvetlenül Oroszországba küldjük, illetve automatikusan feltöltjük a WikiLeaksre.
-                    Szegény Clinton is hogy pórul járt miután bejelentkezett ide!
-                </p>
-            </div>
-        );
-    };
+    return (
+        <div className={css.staticContent}>
+            {type === "terms of service" && renderTermsOfService()}
+            {type === "privacy policy" && renderPrivacyPolicy()}
+        </div>
+    );
 }
