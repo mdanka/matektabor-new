a3eb248a37c4378936a7b8cd7ffd21afe6ff753d
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-11 12:14:18 +0200

    Update functions service

--- src/services/services.ts
+++ src/services/services.ts
@@ -2,23 +2,19 @@ import { FirebaseService } from "./firebaseService";
 import { Store } from "redoodle";
 import { IAppState } from "../store";
 import { FirebaseAuthUiService } from "./firebaseAuthUiService";
-import { FunctionsService } from "./functionsService";
 
 export interface IGlobalServices {
     firebaseService: FirebaseService;
     firebaseAuthUiService: FirebaseAuthUiService;
-    functionsService: FunctionsService;
 }
 
 function getServices(store: Store<IAppState> | undefined): IGlobalServices {
     const firebaseService = new FirebaseService();
     const firebaseApp = firebaseService.getApp();
     const firebaseAuthUiService = new FirebaseAuthUiService(firebaseApp, store);
-    const functionsService = new FunctionsService(firebaseApp);
     return {
         firebaseService,
         firebaseAuthUiService,
-        functionsService,
     };
 }
 
@@ -30,27 +26,5 @@ export function getGlobalServices() {
 
 export function initializeAndGetClientSideServices(store: Store<IAppState>) {
     GLOBAL_SERVICES = getServices(store);
-    createSecretBackupDataCode();
     return GLOBAL_SERVICES;
-}
-
-function createSecretBackupDataCode() {
-    (document as any).backupData = async () => {
-        if (GLOBAL_SERVICES === undefined) {
-            return;
-        }
-        const data = await GLOBAL_SERVICES.functionsService.backupData();
-        const dateString = new Date().toISOString();
-        downloadAsFile(`matektabor-backup-${dateString}.json`, data);
-    };
-}
-
-function downloadAsFile(filename: string, text: string) {
-    const element = document.createElement("a");
-    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
-    element.setAttribute("download", filename);
-    element.style.display = "none";
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
 }
