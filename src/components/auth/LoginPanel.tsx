08d26e2b9f5aa92ddce1922311b1374f372656ac
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-11 18:00:05 +0200

    Replace firebaseui with a manual method

---
+++ src/components/auth/LoginPanel.tsx
@@ -0,0 +1,119 @@
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+import { useAuth, useSigninCheck } from "reactfire";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useSnackbar } from "notistack";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import { LoginWithEmailLink } from "./LoginWithEmailLink";
+import { useHistory } from "react-router";
+import { getNavUrl, Page } from "../../utils/navUtils";
+
+export function LoginPanel(props: { redirectUrl?: string }) {
+    const { redirectUrl } = props;
+    const history = useHistory();
+    const auth = useAuth();
+    const { status: signInCheckStatus, data: signInCheckResult } = useSigninCheck();
+    const { enqueueSnackbar } = useSnackbar();
+    const [isLoggingIn, setIsLoggingIn] = useState<boolean>(false);
+    const [activatedFlow, setActivatedFlow] = useState<"EMAIL_WITH_LINK" | undefined>(undefined);
+
+    const handleAuthSuccess = useCallback(() => {
+        void history.push(redirectUrl ?? getNavUrl[Page.Home]());
+    }, [history, redirectUrl]);
+
+    const handleInitGoogleLogin = useCallback(() => {
+        const execute = async () => {
+            setIsLoggingIn(true);
+            const provider = new GoogleAuthProvider();
+            try {
+                auth.languageCode = "hu";
+                await signInWithPopup(auth, provider);
+                handleAuthSuccess();
+            } catch (error) {
+                enqueueSnackbar("Nem sikerült a bejelentkezés - kérjük próbáld újra!", { variant: "error" });
+                console.error(error);
+                setIsLoggingIn(false);
+            }
+        };
+        void execute();
+    }, [auth, handleAuthSuccess, enqueueSnackbar]);
+
+    const handleGoToHome = useCallback(() => {
+        void history.push(getNavUrl[Page.Home]());
+    }, [history]);
+
+    const handleEmailWithLinkFlowActivated = useCallback(() => {
+        setActivatedFlow("EMAIL_WITH_LINK");
+    }, []);
+
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: 1,
+            }}
+        >
+            {isLoggingIn && (
+                <CircularProgress size={60} />
+            )}
+            {!isLoggingIn && (
+                <>
+                    {signInCheckStatus === "loading" && <CircularProgress size={60} />}
+                    {signInCheckStatus === "success" && signInCheckResult.signedIn && (
+                        <>
+                            <Typography variant="body1" paragraph>
+                                Már be vagy jelentkezve.
+                            </Typography>
+                            <Button onClick={handleGoToHome} variant="contained">
+                                Tovább a kezdőlapra
+                            </Button>
+                        </>
+                    )}
+                    {signInCheckStatus === "success" && !signInCheckResult.signedIn && (
+                        <>
+                            {activatedFlow === undefined && (
+                                <>
+                                    <Typography variant="h4" paragraph>
+                                        Üdvözlünk!
+                                    </Typography>
+                                    <Typography variant="body1" paragraph>
+
+                                        Kattints alább a bejelentkezéshez!
+                                    </Typography>
+                                </>
+                            )}
+                            <Grid2
+                                container
+                                direction="column"
+                                spacing={2}
+                                alignItems="stretch"
+                                sx={{ padding: "2vh 0" }}
+                            >
+                                {activatedFlow === undefined && (
+                                    <Grid2>
+                                        <Button onClick={handleInitGoogleLogin} variant="contained" fullWidth>
+                                            Bejelentkezés Google fiókkal
+                                        </Button>
+                                    </Grid2>
+                                )}
+                                <Grid2>
+                                    <LoginWithEmailLink
+                                        authInfo={{
+                                            auth,
+                                            tenantLanguageCode: "hu",
+                                        }}
+                                        onFlowActivated={handleEmailWithLinkFlowActivated}
+                                        onAuthSuccess={handleAuthSuccess}
+                                    />
+                                </Grid2>
+                            </Grid2>
+                        </>
+                    )}
+                </>
+            )}
+        </Box>
+    );
+}
