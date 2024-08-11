85df169d3840a2e4dc951eb463df1df59ecf5d79
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-11 18:00:42 +0200

    Fix export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,5 @@ export * from "./appHeader";
 export * from "./common";
 export * from "./matektaborApp";
 export * from "./barkochba/barkochbaScreen";
-export * from "./login";
 export * from "./barkochba";
 export * from "./staticContent";
