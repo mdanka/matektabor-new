674b5ba136e181547ca864825efbfb6432c94fca
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-11 12:27:17 +0200

    Fix useEffect deps, replace auth ui service

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,35 +1,24 @@
-import * as React from "react";
-import { getGlobalServices } from "../services";
+import React, { useEffect, useRef } from "react";
 import css from "./loginProtector.module.scss";
+import { useFirebaseAuthUiService } from "../services/useFirebaseAuthUiService";
 
-export interface ILoginProps {
+interface ILoginProps {
     redirectUrl: string | undefined;
 }
 
-export class Login extends React.Component<ILoginProps, {}> {
-    private ref: React.RefObject<HTMLDivElement>;
+export const Login: React.FC<ILoginProps> = ({ redirectUrl }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const { authStart } = useFirebaseAuthUiService();
 
-    constructor(props: ILoginProps) {
-        super(props);
-        this.ref = React.createRef();
-    }
-
-    public componentDidMount() {
-        const { redirectUrl } = this.props;
-        if (this.ref.current == null) {
-            return;
+    useEffect(() => {
+        if (ref.current) {
+            authStart(ref.current, redirectUrl);
         }
-        const globalServices = getGlobalServices();
-        if (globalServices !== undefined) {
-            globalServices.firebaseAuthUiService.authStart(this.ref.current, redirectUrl);
-        }
-    }
+    }, [redirectUrl, authStart]);
 
-    public render() {
-        return (
-            <div className={css.loginScreen}>
-                <div ref={this.ref} />
-            </div>
-        );
-    }
-}
+    return (
+        <div className={css.loginScreen}>
+            <div ref={ref} />
+        </div>
+    );
+};
--- src/services/services.ts
+++ src/services/services.ts
@@ -1,20 +1,15 @@
 import { FirebaseService } from "./firebaseService";
 import { Store } from "redoodle";
 import { IAppState } from "../store";
-import { FirebaseAuthUiService } from "./firebaseAuthUiService";
 
 export interface IGlobalServices {
     firebaseService: FirebaseService;
-    firebaseAuthUiService: FirebaseAuthUiService;
 }
 
 function getServices(store: Store<IAppState> | undefined): IGlobalServices {
     const firebaseService = new FirebaseService();
-    const firebaseApp = firebaseService.getApp();
-    const firebaseAuthUiService = new FirebaseAuthUiService(firebaseApp, store);
     return {
         firebaseService,
-        firebaseAuthUiService,
     };
 }
 
