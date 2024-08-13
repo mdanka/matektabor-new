ea1eb6f0210b16db1ead52d9c41fb5418bde47b9
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-13 12:11:30 +0200

    Vite fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    include: [
-      '@emotion/react', 
-      '@emotion/styled', 
-      '@mui/material/Tooltip',
-      '@mui/material/Unstable_Grid2',
-    ],
-  },  
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin'],
-    },
-  })],
+  plugins: [react()],
 })
