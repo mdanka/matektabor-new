63735c68f8ea717ecfb5911ffba4f7b5f577bf1e
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-15 12:19:05 +0200

    Try with default auth domain

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ declare module "@mui/styles/defaultTheme" {
 
 const FIREBASE_APP_CONFIG = {
     apiKey: "AIzaSyCe-gLA62Z68YVh_8jx-wCXuXksT-ZD3ws",
-    authDomain: window.location.host,
+    authDomain: "barkochba-app.web.app",
     databaseURL: "https://barkochba-app.firebaseio.com",
     projectId: "barkochba-app",
     storageBucket: "barkochba-app.appspot.com",
