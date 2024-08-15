c997354d5a97c2d872f312b6a083a5893053e4b2
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-15 11:46:31 +0200

    PWA, some module scss removal

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'],
+      manifest: {
+        short_name: "Barkochba",
+        name: "Barkochba App",
+        description: "A Gondolkodás Öröme Alapítvány - Matektábor App",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon"
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512"
+          }
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#ffc40d",
+        background_color: "#ffffff"
+      },      
+      devOptions: {
+        enabled: true
+      },
+    }),
+  ],
 })
