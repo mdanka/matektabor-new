5269780293f0f99478ec9ce32cd35ee33df8bd6d
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2024-08-16 21:16:59 +0200

    Remove dark theme

--- src/utils/themeUtils.ts
+++ src/utils/themeUtils.ts
@@ -1,16 +1,4 @@
-import { createTheme, adaptV4Theme } from "@mui/material/styles";
-
-export const DARK_THEME = createTheme(adaptV4Theme({
-    palette: {
-        mode: "dark",
-        primary: {
-            main: "#ffc40d",
-        },
-        secondary: {
-            main: "#1363A5",
-        },
-    },
-}));
+import { createTheme } from "@mui/material/styles";
 
 export const LIGHT_THEME = createTheme({
     palette: {
